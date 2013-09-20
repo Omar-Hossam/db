ee3b1c448ea90485014a317c3969ef74b04e3b85
Omar Hossam <omar.csis101@gmail.com>
2013-09-20 11:27:04 +0200

    remove unuseful files

--- Plugins/su_advancedcameratools/actloader.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/ruby -w
-#
-# Copyright 2012, Trimble Navigation Limited
-#
-# Loads the root files necessary for operation of Advanced Camera Tools
-
-require 'sketchup.rb'
-
-Sketchup::require 'su_advancedcameratools/advancedcameratools_main'
